d570488cafb61f0763a3ae19aa6a7104a2da0c28
Raphael Michel <webmaster@raphaelmichel.de>
2011-11-19 23:25:43 +0100

    Also use fuzzy translations (better than none ...)

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 # POT file
 pot:
 				xgettext *.php -o i18n/template.pot -L PHP --package-name=gfreader --copyright-holder=raphael@geeksfactory.de -n --from-code=utf-8
+locales:
+				msgfmt -o i18n/en_GB/LC_MESSAGES/reader.mo i18n/en_GB/LC_MESSAGES/reader.po -f
+
--- setlocale.php
+++ setlocale.php
@@ -1,6 +1,15 @@
 <?php
-putenv('LC_ALL=de_DE');
-setlocale(LC_ALL, 'de_DE');
+$locale = 'de_DE';
+$locales = array('de_DE', 'en_GB');
+if(isset($_GET['locale']) and in_array($_GET['locale'], $locales)){
+	$locale = $_GET['locale'];
+	setcookie('locale', $locale, time()+3600*24*365);
+}elseif(isset($_COOKIE['locale']) and in_array($_COOKIE['locale'], $locales)){
+	$locale = $_COOKIE['locale'];
+}
+
+putenv('LC_ALL='.$locale);
+setlocale(LC_ALL, $locale);
 bindtextdomain('reader', './i18n');
 textdomain('reader');
 bind_textdomain_codeset('reader', 'utf-8');
