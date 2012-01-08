e9bbbbe9c76362f254e017abd4952447dce562e8
Raphael Michel <webmaster@raphaelmichel.de>
2012-01-08 12:36:30 +0100

    Change favicon when unread posts are available

--- reader.js
+++ reader.js
@@ -33,6 +33,37 @@ function eraseCookie(name) {
 	createCookie(name,"",-1);
 }
 
+var favicon = { // Thanks: http://softwareas.com/dynamic-favicons
+	change: function(iconURL) {
+	  if (arguments.length==2) {
+	    document.title = optionalDocTitle;
+	  }
+	  this.addLink(iconURL, "icon");
+	  this.addLink(iconURL, "shortcut icon");
+	},
+	 
+	addLink: function(iconURL, relValue) {
+	  var link = document.createElement("link");
+	  link.type = "image/x-icon";
+	  link.rel = relValue;
+	  link.href = iconURL;
+	  this.removeLinkIfExists(relValue);
+	  this.docHead.appendChild(link);
+	},
+	 
+	removeLinkIfExists: function(relValue) {
+	  var links = this.docHead.getElementsByTagName("link");
+	  for (var i=0; i<links .length; i++) {
+	    var link = links[i];
+	    if (link.type=="image/x-icon" && link.rel==relValue) {
+	      this.docHead.removeChild(link);
+	      return; // Assuming only one match at most.
+	    }
+	  }
+	},
+	 
+	docHead:document.getElementsByTagName("head")[0]
+}
 function parseunreadcount(data){
 	if(data.error == 'undefined'){
 		$('#wrap').append('<strong>'+lang.errorplsreload+'</strong>');
@@ -54,6 +85,10 @@ function parseunreadcount(data){
 			}
 		});
 		$('title').html('('+data.unread.all+') '+title);
+		if(data.unread.all > 0)
+			favicon.change('somethingnew.ico');
+		else
+			favicon.change('favicon.ico');
 		unread = data.unread.all;
 		window.setTimeout('loadUnreadCount()', 30000);
 	}
