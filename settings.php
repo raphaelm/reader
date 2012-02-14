059c37a829b0e5bdfa837160d9ab69685b61580c
Raphael Michel <webmaster@raphaelmichel.de>
2012-02-14 12:36:10 +0100

    Locale names

--- settings.php
+++ settings.php
@@ -114,7 +114,7 @@ ignoriere diese E-Mail einfach.
 	echo '<form action="settings.php" method="post">
 			<select name="locale">';
 	foreach ($locales as $loc) {
-		echo '<option'.($loc == $me->locale ? ' selected="selected"' : '').'>'.$loc.'</option>';
+		echo '<option'.($loc == $me->locale ? ' selected="selected"' : '').'>'.$loc.' ('.$localenames[$loc].')</option>';
 	}
 	echo '</select>
 			<input type="submit" value="'._('Speichern').'" />
