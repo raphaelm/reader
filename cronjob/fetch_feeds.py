d767db85e4bb0abc9dea3509fe24994748ddc1ad
Raphael Michel <webmaster@raphaelmichel.de>
2012-02-14 15:14:06 +0100

    Multi-Threading bug fixes; Version control system

--- cronjob/fetch_feeds.py
+++ cronjob/fetch_feeds.py
@@ -4,21 +4,15 @@
 import MySQLdb
 import gfrbase
 import time
-
-class GFR_Feedfetcher(gfrbase.GFR):
-	def __init__(self):
-		gfrbase.GFR.__init__(self) # datenbankverbindung aufbauen
-		
-		onemonthago = time.time() - 2592000 # älter als 30 Tage
-		self.cursor.execute("DELETE FROM `feeds_entries` WHERE `timestamp` < %s", (onemonthago,)) # zu alt
-		self.cursor.execute("DELETE FROM `feeds_read` WHERE 0 = (SELECT COUNT(`article_id`) FROM `feeds_entries` WHERE `feeds_entries`.`article_id` = `feeds_read`.`article_id`)") # verwaiste einträge in feeds_read
-		self.cursor.execute("DELETE FROM `sticky` WHERE 0 = (SELECT COUNT(`article_id`) FROM `feeds_entries` WHERE `feeds_entries`.`article_id` = `sticky`.`article_id`)") # verwaiste einträge in sticky
-		self.cursor.execute("DELETE FROM `feeds_subscription` WHERE 0 = (SELECT COUNT(`id`) FROM `feeds` WHERE `feeds`.`id` = `feeds_subscription`.`feedid`) OR 0 = (SELECT COUNT(`id`) FROM `user` WHERE `user`.`id` = `feeds_subscription`.`userid`)") # verwaiste einträge in subscriptions
-		self.feedlist = self.getFeedList()
-		self.getFeeds(4)
 		
 def main():
-	GFR_Feedfetcher()
+	g = gfrbase.GFR()
+	onemonthago = time.time() - 2592000 # älter als 30 Tage
+	g.cursor.execute("DELETE FROM `feeds_entries` WHERE `timestamp` < %s", (onemonthago,)) # zu alt
+	g.cursor.execute("DELETE FROM `feeds_read` WHERE 0 = (SELECT COUNT(`article_id`) FROM `feeds_entries` WHERE `feeds_entries`.`article_id` = `feeds_read`.`article_id`)") # verwaiste einträge in feeds_read
+	g.cursor.execute("DELETE FROM `sticky` WHERE 0 = (SELECT COUNT(`article_id`) FROM `feeds_entries` WHERE `feeds_entries`.`article_id` = `sticky`.`article_id`)") # verwaiste einträge in sticky
+	g.cursor.execute("DELETE FROM `feeds_subscription` WHERE 0 = (SELECT COUNT(`id`) FROM `feeds` WHERE `feeds`.`id` = `feeds_subscription`.`feedid`) OR 0 = (SELECT COUNT(`id`) FROM `user` WHERE `user`.`id` = `feeds_subscription`.`userid`)") # verwaiste einträge in subscriptions
+	g.getFeeds(4)
 	return 0
 
 if __name__ == '__main__':
--- cronjob/gfrbase.py
+++ cronjob/gfrbase.py
@@ -14,8 +14,19 @@ import urlparse
 import urllib
 import socket
 import threading
+import Queue
 
 class GFR:
+	guids = None
+	feedlist = None
+	hashs = None
+	conn = None
+	cursor = None
+	threads = None
+	sqlqueue = None
+	running = True
+	todo = None
+	
 	def __init__(self):
 		socket.setdefaulttimeout(15)
 		try:
@@ -27,29 +38,71 @@ class GFR:
 		except MySQLdb.Error, e:
 			print "Error %d: %s" % (e.args[0], e.args[1])
 			sys.exit(1)
+		
+		self.checkForUpdates()
+				
+	def checkForUpdates(self):
+		updateurl = "http://reader.geeksfactory.de/latest.txt"
+		h = urllib.urlopen(updateurl)
+		c = h.read(22).strip()
+		latest = False
+		if c.startswith('LATESTSTABLE:'):
+			v = c[13:]
+			if len(v) == 8:
+				latest = int(v)
+		own = int(open(config.maindir+'/version.txt').read().strip())
+		
+		if latest > own:
+			print h.read()
+		h.close()
 	
 	def getFeedList(self):
 		self.cursor.execute("SELECT `id`, `name`, `url` FROM `feeds`")
-		return self.cursor.fetchall()
+		self.feedlist = self.cursor.fetchall()
 	
 	def getEntryGuids(self):
 		self.cursor.execute("SELECT `guid`, `contenthash` FROM `feeds_entries`")
 		res = self.cursor.fetchall()
-		ret = []
-		ret2 = []
+		self.guids = set()
+		self.hashs = set()
 		for r in res:
-			ret.append(r[0])
-			ret2.append(r[1])
-		return (tuple(ret), tuple(ret2))
+			self.guids.add(r[0])
+			self.hashs.add(r[1])
 		
-	def getFeeds(self, threads=1):
-		(self.guids, self.hashs) = self.getEntryGuids()
+	def _threaded_sql_exec(self, q, params = None):
+		if self.threads is None:
+			self.cursor.execute(q, params)
+		else:
+			self.sqlqueue.put((q, params))
 		
+	def sqlworker(self):
+		conn = MySQLdb.connect(host = config.dbhostname,
+						   user = config.dbusername,
+						   passwd = config.dbpassword,
+						   db = config.database)
+		cursor = conn.cursor()
+		while self.running:
+			try:
+				item = self.sqlqueue.get(True, 1)
+			except:
+				continue
+			self.cursor.execute(item[0], item[1])
+			conn.commit()
+			self.sqlqueue.task_done()
+			
+		
+	def getFeeds(self, threads=1):	
+		self.getFeedList()
+		
+		if self.guids is None or type(self.guids) is not type(set()):
+			self.getEntryGuids()
+			
 		if threads == 1:
 			for feed in self.feedlist:
 				self.doAFeed(feed)
 		else:
 			self.todo = self.feedlist
+			self.sqlqueue = Queue.Queue()
 			self.threads = []
 			self.todo_lock = threading.Lock()
 			for i in xrange(threads):
@@ -57,17 +110,16 @@ class GFR:
 				t.start()
 				self.threads.append(t)
 				
+			sqlt = threading.Thread(target=self.sqlworker)
+			sqlt.start()
+				
 			for t in self.threads:
 				t.join()
+			
+			self.running = False
+			sqlt.join()
 				
-	def worker(self):
-		
-		conn = MySQLdb.connect(host = config.dbhostname,
-						   user = config.dbusername,
-						   passwd = config.dbpassword,
-						   db = config.database)
-		cursor = conn.cursor()
-		
+	def worker(self):		
 		while True:
 			self.todo_lock.acquire()
 			if len(self.todo) == 0:
@@ -76,9 +128,9 @@ class GFR:
 			feed = self.todo[0]
 			self.todo = self.todo[1:]
 			self.todo_lock.release()
-			self.doAFeed(feed, cursor)
+			self.doAFeed(feed)
 		
-	def doAFeed(self, feed, cursor = None):
+	def doAFeed(self, feed):
 		feed_id = int(feed[0])
 		try:
 			feed_title = feed[1].encode("utf-8", "ignore")
@@ -88,17 +140,13 @@ class GFR:
 			feed_title = feed[1]
 		feed_url = feed[2]
 		try:
-			self.getFeed(feed_id, feed_title, feed_url, cursor)
+			self.getFeed(feed_id, feed_title, feed_url)
 		except KeyboardInterrupt:
 			sys.exit(0)
 		except:
-			print "Error parsing feed %s" % feed_url
+			print "Error parsing feed", feed_id, feed_url
 			
-	def getFeed(self, feed_id, feed_title, feed_url, cursor = None):
-		
-		if cursor is None:
-			cursor = self.cursor
-		
+	def getFeed(self, feed_id, feed_title, feed_url):
 		start = time.time()
 	
 		try:
@@ -118,14 +166,14 @@ class GFR:
 		if f.version == '':
 			return False
 		else:
-			cursor.execute("UPDATE `feeds` Set `lastupdate` = %s WHERE `id` = %s", (time.time(), feed_id))
+			self._threaded_sql_exec("UPDATE `feeds` Set `lastupdate` = %s WHERE `id` = %s", (time.time(), feed_id))
 			
 		try:
 			if f.feed.title.encode("utf-8") != feed_title and not f.feed.title.startswith("http://"): # feed_title ist anders als letztes Mal
 				feed_title = f.feed.title
 				if feed_title.strip() == '':
 					feed_title = feed_url
-				cursor.execute("UPDATE `feeds` Set `name` = %s WHERE `id` = %s", (feed_title, feed_id))
+				self._threaded_sql_exec("UPDATE `feeds` Set `name` = %s WHERE `id` = %s", (feed_title, feed_id))
 		except:
 			#print "Error parsing title for feed %s" % feed_url
 			pass
@@ -159,7 +207,7 @@ class GFR:
 				entry_guid = None
 				try:
 					# The feed provides one? Awesome!
-					entry_guid = entry['id'].encode("utf-8")
+					entry_guid = hashlib.sha1(entry['id'].encode("utf-8")).hexdigest()
 				finally:
 					# The feed doesn't? We choose.
 					if entry_guid is None or len(entry_guid) < 10:
@@ -183,21 +231,23 @@ class GFR:
 				entry_summary = zlib.compress(entry_summary, 9)
 				
 				qp = (feed_id, entry_title, entry_link, entry_guid, entry_contenthash, int(entry_timestamp), entry_summary)
-				
 				if entry_guid not in self.guids: # no duplicates
 					try:
-						cursor.execute("INSERT INTO `feeds_entries` (`feed_id`, `title`, `url`, `guid`, `contenthash`, `timestamp`, `summary`) VALUES (%s, %s, %s, %s, %s, %s, %s)", qp)
+						self._threaded_sql_exec("INSERT INTO `feeds_entries` (`feed_id`, `title`, `url`, `guid`, `contenthash`, `timestamp`, `summary`) VALUES (%s, %s, %s, %s, %s, %s, %s)", qp)
+						self.hashs.add(entry_contenthash)
+						self.guids.add(entry_guid)
 					finally:
 						pass
 				elif entry_contenthash not in self.hashs:
 					try:
-						cursor.execute("REPLACE INTO `feeds_entries` (`feed_id`, `title`, `url`, `guid`, `contenthash`, `timestamp`, `summary`) VALUES (%s, %s, %s, %s, %s, %s, %s)", qp)
+						self._threaded_sql_exec("REPLACE INTO `feeds_entries` (`feed_id`, `title`, `url`, `guid`, `contenthash`, `timestamp`, `summary`) VALUES (%s, %s, %s, %s, %s, %s, %s)", qp)
+						self.hashs.add(entry_contenthash)
 					finally:
 						pass
 						
 		end = time.time()
 		d = end-start
 		if d > 5:
-			cursor.execute("UPDATE `feeds` Set `slower` = 1 WHERE `id` = %s", (feed_id))
+			self._threaded_sql_exec("UPDATE `feeds` Set `slower` = 1 WHERE `id` = %s", (feed_id))
 		else:
-			cursor.execute("UPDATE `feeds` Set `slower` = 0 WHERE `id` = %s", (feed_id))
+			self._threaded_sql_exec("UPDATE `feeds` Set `slower` = 0 WHERE `id` = %s", (feed_id))
