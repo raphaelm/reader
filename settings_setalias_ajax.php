bdc2e8102140256e798ba3e48d10e0c70cc20927
Raphael Michel <webmaster@raphaelmichel.de>
2012-04-01 00:20:07 +0200

    Security- & functionality-relevant bugfix

--- settings_setalias_ajax.php
+++ settings_setalias_ajax.php
@@ -1,5 +1,5 @@
 <?php
 require_once 'includes/dbconnect.php';
 if ($user_id and $_GET['hash'] == sha1($user_id.$salt.date('Ymd'))) {
-	mysql_query("UPDATE feeds_subscription SET alias = '".htmlspecialchars(urldecode($_GET['alias']))."' WHERE userid = ".intval($user_id)." and feedid = ".intval($_GET['id']));
+	mysql_query("UPDATE feeds_subscription SET alias = '".mysql_real_escape_string(htmlspecialchars(urldecode($_GET['alias'])))."' WHERE userid = ".intval($user_id)." and feedid = ".intval($_GET['id']));
 }
